18af63220b153bf39ddc65dd3477e94c7fe249a9
Sk Niyaj Ali <niyaj639@gmail.com>
2025-01-16 20:22:54 +0530

    docs: Updated documentation to improve clarity and consistency (#24)
    * docs: Updated documentation to improve clarity and consistency
    
    Updated the documentation for multiple modules to enhance readability and consistency. The updates include:
    
    - `cmp-android`: Clarified the purpose of the AndroidApp class and its role in initializing dependencies.
    - `cmp-desktop`: Expanded the documentation for the `main` function, detailing its responsibilities in starting the desktop application.
    - `cmp-web`: Improved the documentation for the `main` functions, emphasizing the application setup and initialization process.
    - `cmp-web`: Clarified the purpose and flow of the application's entry point in the `Application.kt` file.
    
    * docs: Updated documentation to improve clarity and consistency
    
    Updated the documentation for multiple modules to enhance readability and consistency. The updates include:
    
    - `cmp-android`: Clarified the purpose of the AndroidApp class and its role in initializing dependencies.
    - `cmp-desktop`: Expanded the documentation for the `main` function, detailing its responsibilities in starting the desktop application.
    - `cmp-web`: Improved the documentation for the `main` functions, emphasizing the application setup and initialization process.
    - `cmp-web`: Clarified the purpose and flow of the application's entry point in the `Application.kt` file.

--- cmp-desktop/src/jvmMain/kotlin/main.kt
+++ cmp-desktop/src/jvmMain/kotlin/main.kt
@@ -16,20 +16,33 @@ import cmp.shared.di.initKoin
 
 /**
  * Main function.
- * This function is used to start the application.
+ * This function is used to start the desktop application.
+ * It performs the following tasks:
+ * 1. Initializes Koin for dependency injection.
+ * 2. Creates a window state to manage the window's state.
+ * 3. Creates a window with a specified title and close request handler.
+ * 4. Calls `SharedApp()` to render the root composable of the application.
+ *
  * @see application
  * @see rememberWindowState
+ * @see Window
+ * @see SharedApp
  */
 fun main() {
     application {
+        // Initializes the Koin dependency injection framework.
         initKoin()
+
+        // Creates a window state to manage the window's state.
         val windowState = rememberWindowState()
+
+        // Creates a window with a specified title and close request handler.
         Window(
             onCloseRequest = ::exitApplication,
             state = windowState,
             title = "MifosAppTemplate",
         ) {
-            // Set the content of the window.
+            // Sets the content of the window.
             SharedApp()
         }
     }

a1fa422555ec86d8d10c0ba36636612468154a1f
Arin Yadav <arinyadav98@gmail.com>
2026-02-24 01:42:30 +0530

    Fix Room implementation and Koin DatabaseModule integration

--- core/database/src/wasmJsMain/kotlin/org/mifos/core/database/di/DatabaseModule.wasmJs.kt
+++ core/database/src/wasmJsMain/kotlin/org/mifos/core/database/di/DatabaseModule.wasmJs.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Mifos Initiative
+ * Copyright 2026 Mifos Initiative
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -9,13 +9,8 @@
  */
 package org.mifos.core.database.di
 
-import org.koin.core.module.Module
 import org.koin.dsl.module
-import org.mifos.core.database.AppDatabase
 
-val DatabaseModule = module {
-    includes(platformModule)
-    single { get<AppDatabase>().sampleDao }
+actual val platformModule = module {
+    // EMPTY — no DB on WasmJs
 }
-
-expect val platformModule: Module
